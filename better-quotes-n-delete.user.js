232216d2fbcd4153188c68cd0ebccc41d0f98079
lester-td <lester.t.d@outlook.com>
2025-09-01 01:34:32 +0800

    set up auto update

--- better-quotes-n-delete.user.js
+++ better-quotes-n-delete.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         Flashii Chat - Better Quotes & Delete Button
+// @namespace    https://patchii.net/lester/flashii-chat-userscripts
 // @version      4.0
 // @description  Adds message quoting and preview, via button and timestamp. Adds delete button to own messages.
 // @author       lester
 // @match        *://chat.flashii.net/*
 // @grant        none
+// @downloadURL  https://patchii.net/lester/flashii-chat-userscripts/raw/branch/trunk/better-quotes-n-delete.user.js
+// @updateURL    https://patchii.net/lester/flashii-chat-userscripts/raw/branch/trunk/better-quotes-n-delete.meta.js
 // ==/UserScript==
 
 (() => {
---
+++ file-upload-progress-bar.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Flashii Chat - File Upload Progress Bar
+// @namespace    https://patchii.net/lester/flashii-chat-userscripts
+// @version      1.1
+// @description  Show progress bar beside Spoiler button during file upload
+// @author       lester
+// @match        *://chat.flashii.net/*
+// @grant        none
+// @downloadURL  https://patchii.net/lester/flashii-chat-userscripts/raw/branch/trunk/file-upload-progress-bar.user.js
+// @updateURL    https://patchii.net/lester/flashii-chat-userscripts/raw/branch/trunk/file-upload-progress-bar.meta.js
+// ==/UserScript==
